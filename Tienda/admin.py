c3c38d9d0019d2bac8d227c0e1be118d649e14c9
NicollMendezChica <80913505+NicollMendezChica@users.noreply.github.com>
2021-09-28 20:39:40 -0500

    Update admin.py

--- Tienda/admin.py
+++ Tienda/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from Tienda.models import InicioSesion,Usuario,Tienda,Movimiento,Producto
 
 # Register your models here.
+
+admin.site.register(InicioSesion)
+admin.site.register(Usuario)
+admin.site.register(Tienda)
+admin.site.register(Movimiento)
+admin.site.register(Producto)
