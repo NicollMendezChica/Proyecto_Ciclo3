c095fd5fc3fdca5b71d81801866e9e64fabde3dc
NicollMendezChica <80913505+NicollMendezChica@users.noreply.github.com>
2021-09-30 11:26:03 -0500

    Update views.py
    Definición de vistas para archivos html.

--- Tienda/views.py
+++ Tienda/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def formulario(request):
+    return render(request, "Formulario.html")
+
+def TablaProductos(request):
+    return render(request, "TablaProductos.html")
