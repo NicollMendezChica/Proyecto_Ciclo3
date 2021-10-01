37bcc05fe7611ff9f4f552b32ed1f81047dabc1a
NicollMendezChica <80913505+NicollMendezChica@users.noreply.github.com>
2021-09-30 20:52:37 -0500

    Update views.py

--- Tienda/views.py
+++ Tienda/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
 from Tienda.models import *
+from Tienda.Forms import *
 
 # Create your views here.
 
@@ -10,3 +13,17 @@ def TablaProductos(request):
     productos=Producto.objects.all()  # Equivalente a decir SELECT * FROM PRODUCTOS
     # print(productos)
     return render(request,"TablaProductos.html", {"misProductos": productos})
+
+def crearProds(request):
+    if request.method == 'GET':
+        form = ProductoFrm()
+    else:
+        form = ProductoFrm(request.POST)
+
+    contexto = {'Formulario':form}
+    
+    if form.is_valid():
+        form.save()
+        return redirect('ListadoTabla')
+
+    return render(request,'CrearProductos.html', contexto)
