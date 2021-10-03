f9fe8a6ac77cc37306a374c754217f24488a990b
NicollMendezChica <80913505+NicollMendezChica@users.noreply.github.com>
2021-10-03 15:42:39 -0500

    Update views.py
    Creación de vista para crear productos.

--- Tienda/views.py
+++ Tienda/views.py
@@ -19,11 +19,34 @@ def CrearProductos(request):
         form = ProductoFrm()
     else:
         form = ProductoFrm(request.POST)
-
     contexto = {'Formulario':form}
-    
     if form.is_valid():
         form.save()
-        return redirect('ListadoTabla')
-
+        return redirect('TablaProductos')
     return render(request,'CrearProductos.html', contexto)
+
+def vistaAccionFrm(request):
+    if request.GET['Nombre']:  
+    #mensaje = request.GET["Nombre"] + " NO se encuentra entre nuestos Productos"
+        miVarName=request.GET["Nombre"]
+        misprods=Producto.objects.filter(nombreProducto__icontains=miVarName) # WHERE nomProd Like '%miVarName%'
+        return render(request,"Resultados.html", {"articulos":misprods, "query":miVarName})
+    else:
+        mensaje = "No hay información..."
+    return HttpResponse(mensaje)
+
+#def listarProds(request):
+#    productos=Productos.objects.all()  # Equivalente a decir SELECT * FROM PRODUCTOS
+    # print(productos)
+#    return render(request,"listarprods.html", {"misProductos": productos})
+
+# form = ProductoFrm()
+    # # return render(request,"crearprods.html", {'form': form})
+    # context = {
+    #     'form':form
+    # }
+    # return render(request,"crearprods.html", contexto)
+    # else:
+    #     form = ProductoFrm(request.POST)
+    #     print(form)
+    # return render(request,"crearprods.html", context)
