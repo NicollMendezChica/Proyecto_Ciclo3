8a5c331e557fc20c0f4816fb99205769d7c3ef41
NicollMendezChica <80913505+NicollMendezChica@users.noreply.github.com>
2021-10-04 21:24:55 -0500

    Update Forms.py
    Creación del Form para los usuarios.

--- Tienda/Forms.py
+++ Tienda/Forms.py
@@ -5,3 +5,8 @@ class ProductoFrm(forms.ModelForm):
     class Meta:
         model=Producto
         fields='__all__'                # fields=('nomProd', 'Precio', 'Color',)
+
+class UsuarioFrm(forms.ModelForm):
+    class Meta:
+        model=Usuario
+        fields='__all__'
